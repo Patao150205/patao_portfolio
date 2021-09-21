b1d9e9a30c24470ef2f1994478556757d1faf733
Patao150205 <imaimizuki150205@yahoo.co.jp>
2021-09-22 07:45:58 +0900

    implement animation

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,17 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
-gulp.task('server', function (done) {
+gulp.task('server', function () {
   browserSync.init({
     proxy: 'http://localhost:8888',
     reloadOnRestart: true,
   });
-
-  done();
 });
 
-gulp.task('watch', function (done) {
+gulp.task('watch', function () {
   gulp.watch('./public/**/*').on('change', () => {
     browserSync.reload();
   });
-  done();
 });
 
 gulp.task('default', gulp.parallel('server', 'watch'));
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '201aef0a3c66aa6cfed64d6ce6c3ca00ada2a4a6',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '201aef0a3c66aa6cfed64d6ce6c3ca00ada2a4a6',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.5.1',
+            'version' => '6.5.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'dd803df5ad7492e1b40637f7ebd258fee5ca7355',
+            'dev_requirement' => false,
+        ),
+        'vlucas/valitron' => array(
+            'pretty_version' => 'v1.4.10',
+            'version' => '1.4.10.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../vlucas/valitron',
+            'aliases' => array(),
+            'reference' => '5ebd5d7af82cc0a9d798e816a2ecad63f1b59ceb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
